59790b9d178791bd2b3ce4c99e35eb1ee9f953de
Victor131boop <84921101+Victor131boop@users.noreply.github.com>
2025-01-12 19:29:25 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Poți adăuga interactivitate, cum ar fi un mesaj de întâmpinare
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Site-ul echipei de robotică este gata!");
+});
